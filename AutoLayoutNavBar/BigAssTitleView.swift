470a1c3da1b5f5da3ada9f17a6c25d58fc1b73b0
ryanbaldwin <ryanbaldwin@gmail.com>
2017-10-14 23:42:08 -0400

    big bang, son

---
+++ AutoLayoutNavBar/BigAssTitleView.swift
@@ -0,0 +1,13 @@
+//
+//  BigAssTitleView.swift
+//  AutoLayoutNavBar
+//
+//  Created by Ryan Baldwin on 2017-10-14.
+//  Copyright © 2017 bunnyhug.me. All rights reserved.
+//
+
+import UIKit
+
+class BigAssTitleView: UIView {
+
+}
---
+++ AutoLayoutNavBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutNavBar
+//
+//  Created by Ryan Baldwin on 2017-10-14.
+//  Copyright © 2017 bunnyhug.me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
